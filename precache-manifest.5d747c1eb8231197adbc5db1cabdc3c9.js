1508116205d14ccb938c169ce56ac85f2fe0ad0d
Cypher <adityasaha444@gmail.com>
2020-09-15 15:51:48 +0530

    Updates

---
+++ precache-manifest.5d747c1eb8231197adbc5db1cabdc3c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cffe4c21bb94e62dfc628d7339708c36",
+    "url": "/Photo_editor/index.html"
+  },
+  {
+    "revision": "354478f2ca41b5a55b74",
+    "url": "/Photo_editor/static/css/main.22f883cb.chunk.css"
+  },
+  {
+    "revision": "a2166436a150ee6c33a6",
+    "url": "/Photo_editor/static/js/2.e593b07c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Photo_editor/static/js/2.e593b07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "354478f2ca41b5a55b74",
+    "url": "/Photo_editor/static/js/main.8d04feb6.chunk.js"
+  },
+  {
+    "revision": "079f919436f6f7b6016a",
+    "url": "/Photo_editor/static/js/runtime-main.0ba92906.js"
+  },
+  {
+    "revision": "ad8d61bff6fcfa8f1abde26898d2e8aa",
+    "url": "/Photo_editor/static/media/logo192.ad8d61bf.png"
+  }
+]);
